94cd43ab138d6024bc76a5c37400e6c4f93d50cb
RimshaWazir <rimshawazir48@gmail.com>
2023-11-30 16:44:46 +0500

    modification

--- lib/SRC/Presentation/Screens/Splash/splash_screen.dart
+++ lib/SRC/Presentation/Screens/Splash/splash_screen.dart
@@ -1,15 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:name_generator/SRC/Presentation/Screens/Home/home_test.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 50), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const MyHomePage(
+                  title: 'jkdhfkshdf',
+                )),
+      );
+    });
+
     return Scaffold(
       body: Center(
         child: Image.asset(
           "assets/Images/logo.webp",
           fit: BoxFit.cover,
+          height: 106,
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:name_generator/SRC/Presentation/Screens/Splash/splash_screen.dart';
 
-import 'SRC/Presentation/Screens/Home/home_test.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,13 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Name Generator',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
+    return const MaterialApp(
+      title: 'STC HEALTH',
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
